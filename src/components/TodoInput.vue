cc3d40b9818fba7c1518c6cd5c0a53b31098c662
Sujin92 <psujin831@gmail.com>
2018-08-07 21:38:42 +0900

    first commit

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="input-box shadow">
+        <input type="text" v-model="todoItem" placeholder="할 일을 입력하세요."
+        v-on:keyup.enter="addTodo">
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+        </span>
+
+        <modal v-if="showModal" @close="showModal = false">
+            <h3 slot="header">WARNING</h3>
+            <span slot="footer" @click="showModal = false">
+                할 일을 입력하세요.
+                <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+            </span>
+        </modal>
+    </div>  
+</template>
+
+<script>
+import ErrorModal from '@/components/ErrorModal.vue'
+
+export default {
+    data() {
+        return {
+            todoItem : '',
+            showModal: false
+        }
+    },
+    methods: {
+        addTodo() {
+            if (this.todoItem !== "") {
+                var value = this.todoItem && this.todoItem.trim();
+                this.$emit('addTodo', value);
+                this.clearInput();
+            } else {
+                this.showModal = !this.showModal
+            }
+        },
+        clearInput() {
+            this.todoItem = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+    input:focus {
+        outline: none;
+    }
+    
+    .input-box {
+        background: white;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 5px;
+    }
+
+    .input-box input {
+        border-style: none;
+        font-size: 2rem;
+    }
+
+    .addContainer {
+        float: right;
+        background: linear-gradient(to right, #6478FB, #8763FB);
+        display: block;
+        width: 5rem;
+        border-radius: 0 5px 5px 0;
+    }
+
+    .addBtn {
+        color: white;
+        vertical-align: middle;
+    }
+</style>
